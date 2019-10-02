4e0185f3ab9344312d0b9e264ddced7dfd5823b6
Clint Milner <clinton.milner@rebasoft.net>
2019-10-02 07:56:51 +0100

    Updates

---
+++ precache-manifest.cc49453fb1c3a661b8174fa54b443304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedd3ef9fe3bba12e8040ab6b6505591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62271b9898c5afc683e",
+    "url": "/static/css/main.f01269cd.chunk.css"
+  },
+  {
+    "revision": "07945d4a77a36bddb005",
+    "url": "/static/js/2.a01cbe7c.chunk.js"
+  },
+  {
+    "revision": "a62271b9898c5afc683e",
+    "url": "/static/js/main.12412efe.chunk.js"
+  },
+  {
+    "revision": "fda8632f94463b905c8d",
+    "url": "/static/js/runtime-main.d6d12294.js"
+  },
+  {
+    "revision": "d6a0cc6f7b08f4072d467904b0df8c17",
+    "url": "/static/media/Marvel-Regular.d6a0cc6f.woff"
+  },
+  {
+    "revision": "ebaf3a367f056dfd8c0961b7666256fa",
+    "url": "/static/media/Marvel-Regular.ebaf3a36.woff2"
+  }
+]);
